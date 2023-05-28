996cb00c792b18ccc36200e9b89eb7cf7191a611
Vladimir Zhukov <zhukovvlad@yandex.ru>
2023-05-29 00:06:56 +0300

    Creating pd dateframe for fetching prices

--- program/func_public.py
+++ program/func_public.py
@@ -9,8 +9,58 @@ from pprint import pprint
 # Get relevant time periods for ISO from and to
 ISO_TIMES = get_iso_times()
 
-pprint(ISO_TIMES)
+# Get Candles Historical
+
+
+def get_candles_historical(client, market):
+
+    # Define output
+    close_prices = []
+
+    # Exctract historical price data for each timeframe
+    for timeframe in ISO_TIMES.keys():
+        # Confirm times needed
+        tf_obj = ISO_TIMES[timeframe]
+        from_iso = tf_obj["from_iso"]
+        to_iso = tf_obj["to_iso"]
+
+        # Protect rate limits
+        time.sleep(0.2)
+
+        # Get data
+        candles = client.public.get_candles(
+            market=market,
+            resolution=RESOLUTION,
+            from_iso=from_iso,
+            to_iso=to_iso,
+            limit=100
+        )
+
+        # Structure data
+        for candle in candles.data["candles"]:
+            close_prices.append(
+                {"datetime": candle["startedAt"], market: candle["close"]})
+
+    # Construct and return data
+    close_prices.reverse()
+    return close_prices
 
 # Construct market prices
+
+
 def construct_market_prices(client):
-    pass
+    # Declare variables
+    tradeable_markets = []
+    markets = client.public.get_markets()
+
+    # Find tradeable pairs
+    for market in markets.data["markets"].keys():
+        market_info = markets.data["markets"][market]
+        if market_info["status"] == "ONLINE" and market_info["type"] == "PERPETUAL":
+            tradeable_markets.append(market)
+
+    # Set initial Dateframe
+    close_prices = get_candles_historical(client, tradeable_markets[0])
+    df = pd.DataFrame(close_prices)
+    df.set_index("datetime", inplace=True)
+    print(df.head())
