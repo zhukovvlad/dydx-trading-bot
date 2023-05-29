d8aedc8d44fdca46fdc2f221f40f5bc3c2b6c3e1
Vladimir Zhukov <zhukovvlad@yandex.ru>
2023-05-29 13:33:59 +0300

    Implemented function for creating table with market prices

--- program/func_public.py
+++ program/func_public.py
@@ -63,4 +63,22 @@ def construct_market_prices(client):
     close_prices = get_candles_historical(client, tradeable_markets[0])
     df = pd.DataFrame(close_prices)
     df.set_index("datetime", inplace=True)
-    print(df.head())
+
+    # Append other prices to DateFrame
+    # You can limit the amount to loop though here to save item in development
+    for market in tradeable_markets[1:5]:
+        close_prices_add = get_candles_historical(client, market)
+        df_add = pd.DataFrame(close_prices_add)
+        df_add.set_index("datetime", inplace=True)
+        df = pd.merge(df, df_add, how="outer", on="datetime", copy=False)
+        del df_add
+
+    # Check any columns with Nans
+    nans = df.columns[df.isna().any()].tolist()
+    if len(nans) > 0:
+        print("Dropping columns: ")
+        print(nans)
+        df.drop(columns=nans, inplace=True)
+
+    # Return Result
+    return df
